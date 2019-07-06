6d82c20af96b51b554d234598b4e7a6c59c7622c
ARVO\andrew.koltyakov <andrew.koltyakov@arvosys.com>
2019-07-07 02:08:06 +0300

    Azure Function bundling

--- src/05-Azure-Functions/webpack.config.js
+++ src/05-Azure-Functions/webpack.config.js
@@ -1,18 +1,1 @@
-const nodeExternals = require('webpack-node-externals');
-
-module.exports = (config, webpack) => {
-  if (!config.module) {
-    config.module = {};
-  }
-  if (!config.module.rules) {
-    config.module.rules = [];
-  }
-  config.module.rules.push(
-    {
-      test: /rx\.lite\.aggregates\.js/,
-      use: 'imports-loader?define=>false'
-    }
-  );
-  config.externals = [ nodeExternals() ];
-  return config;
-};
+module.exports = (config) => config;
