a363f2e71c13d97d164acbcaeedf75a5e43a0bc0
ARVO\andrew.koltyakov <andrew.koltyakov@arvosys.com>
2019-07-06 21:47:20 +0300

    Provisioning schema fix

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,6 +1,9 @@
 $ConfigPath = "$PSScriptRoot\..\config\private.json";
 $SchemaPath = "$PSScriptRoot\template.xml";
 
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force;
+Set-PnPTraceLog -on -level Debug;
+
 $Context = Get-Content $ConfigPath -Encoding UTF8 | ConvertFrom-Json;
 
 $Connection = Connect-PnPOnline -Url $Context.siteUrl -ReturnConnection;
